e4ed314c51f5d6c67e0b769238d09fe1c6c479da
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2022-09-23 10:32:03 +0100

    tar and gzip output file

--- src/script.sh
+++ src/script.sh
@@ -7,7 +7,7 @@ dx-download-all-inputs # download inputs from json
 # resources folder will not exist on worker, so removed here.
 mkdir /home/dnanexus/genomeDir
 mkdir /home/dnanexus/reference_genome
-mkdir -p /home/dnanexus/out/output_indices
+mkdir -p /home/dnanexus/out/output_indices/output_indices
 
 tar xvzf /home/dnanexus/in/sentieon_tar/sentieon-genomics-*.tar.gz -C /usr/local # unpack tar
 tar xvzf /home/dnanexus/in/reference_genome/*tar.gz -C /home/dnanexus/reference_genome
@@ -26,9 +26,12 @@ NUMBER_THREADS=32
 export REFERENCE=/home/dnanexus/reference_genome/*.fa  # Reference genome, standard GRCh38
 GTF=/home/dnanexus/in/gtf_file/*gtf  # Input .gtf annotation file
 READ_LENGTH_MINUS_1=100
-OUTPUT_DIR=/home/dnanexus/out/output_indices
+OUTPUT_DIR=/home/dnanexus/out/output_indices/output_indices
 
 # Run STAR command to generate genome indices
 sentieon STAR --runThreadN ${NUMBER_THREADS} --runMode genomeGenerate --genomeDir ${OUTPUT_DIR} --genomeFastaFiles ${REFERENCE} --sjdbGTFfile ${GTF} --sjdbOverhang ${READ_LENGTH_MINUS_1}
+
+# Tar and gzip output file
+tar -czvf output_indices.tar.gz /home/dnanexus/out/output_indices/output_indices
 
 dx-upload-all-outputs
