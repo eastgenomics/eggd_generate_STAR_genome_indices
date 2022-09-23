28ba089f78e994202a8ad42c66b44c113d819a3d
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2022-09-23 16:57:27 +0100

    commenting

--- src/script.sh
+++ src/script.sh
@@ -4,14 +4,14 @@ set -exo pipefail #if any part goes wrong, job will fail
 
 dx-download-all-inputs # download inputs from json
 
-# resources folder will not exist on worker, so removed here.
 mkdir /home/dnanexus/genomeDir
 mkdir /home/dnanexus/reference_genome
 mkdir /home/dnanexus/output_indices
 mkdir /home/dnanexus/gtf
 mkdir -p /home/dnanexus/out/output_indices/
 
-tar xvzf /home/dnanexus/in/sentieon_tar/sentieon-genomics-*.tar.gz -C /usr/local # unpack tar
+# Unpack tarred input files and decompress gzipped files
+tar xvzf /home/dnanexus/in/sentieon_tar/sentieon-genomics-*.tar.gz -C /usr/local
 tar xvzf /home/dnanexus/in/reference_genome/*tar.gz -C /home/dnanexus/reference_genome
 gunzip /home/dnanexus/in/gtf_file/*.gz
 
