0d0bd8b1054d193569a555d68255356bf7a969eb
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2022-09-23 16:54:19 +0100

    added to readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # eggd_make_genome_indices
+
+## What does this app do?
+This app uses a .gtf file of transcripts and the Sentieon STAR tool to generate genome indices, which will annotate a b38 reference genome.
+
+## What inputs are required for this app to run?
+* `--sentieon_tar`: (file) Tarballed Sentieon package. Currently defaults to use Sentieon 202112.05
+* `--reference_genome`:(file) Tarballed GRCh38 reference genome FASTA + index. Current defaults to use GRCh38.no_alt_analysis_set_chr_mask21.fasta-index.tar.gz in 001_Reference
+* `--gtf_file`: (file) File providing gene transcript information. Currently defaults to the GENCODE gtf v41 (gencode.v41.annotation.gtf.gz)
+
+## How does this app work?
+eggd_make_genome_indices takes an input .gtf file of transcript data, and a reference geneome. It creates genome indices.
+
+## What does this app output?
+eggd_make_genome_indices outputs a gzipped file of genome indices.
+
+## Notes
+* This app is not ready for production use
+
+## This app was made by East GLH
--- src/script.sh
+++ src/script.sh
@@ -8,10 +8,12 @@ dx-download-all-inputs # download inputs from json
 mkdir /home/dnanexus/genomeDir
 mkdir /home/dnanexus/reference_genome
 mkdir /home/dnanexus/output_indices
-mkdir -p /home/dnanexus/out/output_indices/output_indices
+mkdir /home/dnanexus/gtf
+mkdir -p /home/dnanexus/out/output_indices/
 
 tar xvzf /home/dnanexus/in/sentieon_tar/sentieon-genomics-*.tar.gz -C /usr/local # unpack tar
 tar xvzf /home/dnanexus/in/reference_genome/*tar.gz -C /home/dnanexus/reference_genome
+gunzip /home/dnanexus/in/gtf_file/*.gz
 
 source /home/dnanexus/license_setup.sh # run license setup script
 
@@ -21,12 +23,16 @@ SENTIEON_BIN_DIR=$(echo $SENTIEON_INSTALL_DIR/bin)
 
 export PATH="$SENTIEON_BIN_DIR:$PATH"
 
+# NUMBER_THREADS input to STAR needs the number of cores on the server node
+# This can be extracted from the DNAnexus instance type
+INSTANCE=$(dx describe --json $DX_JOB_ID | jq -r '.instanceType')  # Extract instance type
+
 ## Generate genome indices
 # Define input variables for STAR command
-NUMBER_THREADS=32
+NUMBER_THREADS=${INSTANCE##*_x}
 export REFERENCE=/home/dnanexus/reference_genome/*.fa  # Reference genome, standard GRCh38
 GTF=/home/dnanexus/in/gtf_file/*gtf  # Input .gtf annotation file
-READ_LENGTH_MINUS_1=100
+READ_LENGTH_MINUS_1=99
 OUTPUT_DIR=/home/dnanexus/output_indices
 
 # Run STAR command to generate genome indices
